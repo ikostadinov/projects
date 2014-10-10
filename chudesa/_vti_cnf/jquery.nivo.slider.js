6cd92ce0e175c15111ddf458bc2ec7d3799a8fcd
Ivan Kostadinov <ikostadinov93@gmail.com>
2014-10-11 01:39:52 +0300

    new 7 world wonders
    school project about new 7 world wonders

---
+++ chudesa/_vti_cnf/jquery.nivo.slider.js
@@ -0,0 +1,6 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|13 Dec 2011 07:43:44 -0000
+vti_extenderversion:SR|6.0.2.8161
+vti_cacheddtm:TX|13 Dec 2011 07:43:44 -0000
+vti_filesize:IR|24996
+vti_backlinkinfo:VX|
---
+++ chudesa/_vti_cnf/jquery.nivo.slider.pack.js
@@ -0,0 +1,6 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|13 Dec 2011 07:44:28 -0000
+vti_extenderversion:SR|6.0.2.8161
+vti_cacheddtm:TX|13 Dec 2011 07:44:28 -0000
+vti_filesize:IR|11561
+vti_backlinkinfo:VX|chudesa/chichen_ica.html chudesa/petra.html chudesa/machu_pikchu.html chudesa/redeemer.html chudesa/stena.html chudesa/tadj_mahal.html chudesa/kolizeum.html
