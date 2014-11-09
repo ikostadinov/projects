3f9623740d43627e6f9a0a200f3099f4a36e5e26
Ivan Kostadinov <ikostadinov93@gmail.com>
2014-11-10 00:08:15 +0200

    top 5 destinations

---
+++ web/top 5 destinations/js/my.js
@@ -0,0 +1,215 @@
+// JavaScript Document
+$(document).ready(function(){
+				//paris
+				$(".infoparis").click(function () {
+				$("#load").load("infoparis.html");
+				
+							});
+							
+
+			
+				$(".hotelsparis").click(function () {
+				$("#load").load("hotelsparis.html");
+							});
+			
+				$(".whattoseeparis").click(function () {
+				$("#load").load("whattoseeparis.html");
+							});
+			
+				$(".restaurantsparis").click(function () {
+				$("#load").load("restaurantsparis.html");
+							});
+				$(".funparis").click(function () {
+				$("#load").load("funparis.html");
+							});
+							//paris dutch
+							$(".infoparis_dutch").click(function () {
+				$("#load").load("infoparis_dutch.html");
+				
+							});						
+
+			
+				$(".hotelsparis_dutch").click(function () {
+				$("#load").load("hotelsparis_dutch.html");
+							});
+			
+				$(".whattoseeparis_dutch").click(function () {
+				$("#load").load("whattoseeparis_dutch.html");
+							});
+			
+				$(".restaurantsparis_dutch").click(function () {
+				$("#load").load("restaurantsparis_dutch.html");
+							});
+				$(".funparis_dutch").click(function () {
+				$("#load").load("funparis_dutch.html");
+							});
+							
+							
+							
+							
+							//barcelona
+				$(".infobar").click(function () {
+				$("#load").load("infobar.html");
+				
+							});
+				
+				$(".hotelsbar").click(function () {
+				$("#load").load("hotelsbar.html");
+							});
+			
+				$(".whattoseebar").click(function () {
+				$("#load").load("whattoseebar.html");
+							});
+			
+				$(".restaurantsbar").click(function () {
+				$("#load").load("restaurantsbar.html");
+							});
+				$(".funbar").click(function () {
+				$("#load").load("funbar.html");
+							});
+							//barcelona dutch
+							$(".infobar_dutch").click(function () {
+				$("#load").load("infobar_dutch.html");
+				
+							});
+				
+				$(".hotelsbar_dutch").click(function () {
+				$("#load").load("hotelsbar_dutch.html");
+							});
+			
+				$(".whattoseebar_dutch").click(function () {
+				$("#load").load("whattoseebar_dutch.html");
+							});
+			
+				$(".restaurantsbar_dutch").click(function () {
+				$("#load").load("restaurantsbar_dutch.html");
+							});
+				$(".funbar_dutch").click(function () {
+				$("#load").load("funbar_dutch.html");
+							});
+							//zermatt
+							$(".infozermatt").click(function () {
+				$("#load").load("infozermatt.html");
+				
+							});
+				
+				$(".hotelszermatt").click(function () {
+				$("#load").load("hotelszermatt.html");
+							});
+			
+				$(".whattoseezermatt").click(function () {
+				$("#load").load("whattoseezermatt.html");
+							});
+			
+				$(".restaurantszermatt").click(function () {
+				$("#load").load("restaurantszermatt.html");
+							});
+				$(".funzermatt").click(function () {
+				$("#load").load("funzermatt.html");
+							});
+							//zermatt dutch
+							$(".infozermatt_dutch").click(function () {
+				$("#load").load("infozermatt_dutch.html");
+				
+							});
+				
+				$(".hotelszermatt_dutch").click(function () {
+				$("#load").load("hotelszermatt_dutch.html");
+							});
+			
+				$(".whattoseezermatt_dutch").click(function () {
+				$("#load").load("whattoseezermatt_dutch.html");
+							});
+			
+				$(".restaurantszermatt_dutch").click(function () {
+				$("#load").load("restaurantszermatt.html");
+							});
+				$(".funzermatt_dutch").click(function () {
+				$("#load").load("funzermatt_dutch.html");
+							});
+							
+							
+							
+							//venice
+							$(".infoven").click(function () {
+				$("#load").load("infoven.html");
+				
+							});
+				
+				$(".hotelsven").click(function () {
+				$("#load").load("hotelsven.html");
+							});
+			
+				$(".whattoseeven").click(function () {
+				$("#load").load("whattoseeven.html");
+							});
+			
+				$(".restaurantsven").click(function () {
+				$("#load").load("restaurantsven.html");
+							});
+				$(".funven").click(function () {
+				$("#load").load("funven.html");
+							});
+							//venice dutch
+								$(".infoven_dutch").click(function () {
+				$("#load").load("infoven_dutch.html");
+				
+							});
+				
+				$(".hotelsven_dutch").click(function () {
+				$("#load").load("hotelsven_dutch.html");
+							});
+			
+				$(".whattoseeven_dutch").click(function () {
+				$("#load").load("whattoseeven_dutch.html");
+							});
+			
+				$(".restaurantsven_dutch").click(function () {
+				$("#load").load("restaurantsven.html");
+							});
+				$(".funven_dutch").click(function () {
+				$("#load").load("funven_dutch.html");
+							});
+							//santorini
+							$(".infosantorini").click(function () {
+				$("#load").load("infosantorini.html");
+				
+							});
+				
+				$(".hotelssantorini").click(function () {
+				$("#load").load("hotelssantorini.html");
+							});
+			
+				$(".whattoseesantorini").click(function () {
+				$("#load").load("whattoseesantorini.html");
+							});
+			
+				$(".restaurantssantorini").click(function () {
+				$("#load").load("restaurantssantorini.html");
+							});
+				$(".funsantorini").click(function () {
+				$("#load").load("funsantorini.html");
+							});
+							//santorini dutch
+										$(".infosantorini_dutch").click(function () {
+				$("#load").load("infosantorini_dutch.html");
+				
+							});
+				
+				$(".hotelssantorini_dutch").click(function () {
+				$("#load").load("hotelssantorini_dutch.html");
+							});
+			
+				$(".whattoseesantorini_dutch").click(function () {
+				$("#load").load("whattoseesantorini_dutch.html");
+							});
+			
+				$(".restaurantssantorini_dutch").click(function () {
+				$("#load").load("restaurantssantorini_dutch.html");
+							});
+				$(".funsantorini_dutch").click(function () {
+				$("#load").load("funsantorini_dutch.html");
+							});
+							
+							
+			})
