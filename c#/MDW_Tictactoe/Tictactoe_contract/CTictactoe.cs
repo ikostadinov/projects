df2172342dcf49ec49ab0f34f848c73d33128774
Ivan Kostadinov <ikostadinov93@gmail.com>
2014-11-10 00:16:12 +0200

    Tic-Tac-Toe CSA

---
+++ c#/MDW_Tictactoe/Tictactoe_contract/CTictactoe.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Tictactoe_contract
+{
+   [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single, ConcurrencyMode = ConcurrencyMode.Reentrant)]
+    public class CTictactoe : ITictactoe
+    {
+       Action<string> e_Joingame = delegate { };
+        List<Player> listOfPlayers = new List<Player>();
+
+
+
+        public void MakeMove(string username, int buttonId, string XO)
+        {
+            foreach (Player p in listOfPlayers)
+            {
+                if (p.PlayerName != username)
+                {
+                    p.callback.OnMoveCallback(buttonId, XO);
+                }
+            }
+        }
+
+       
+
+        public void Chat(string username, string message)
+        {
+            foreach (Player p in listOfPlayers)
+            {
+                if (p.PlayerName != username)
+                {
+                    p.callback.OnChatCallback(username,message);
+                }
+            }
+        }
+
+        //public void CountWins(string username,int wins)
+        //{
+        //    //string info = "";
+        //    foreach (Player p in listOfPlayers)
+        //    {
+               
+                
+        //            p.callback.WinsCallBack(username, wins);
+        //            //info= p.PlayerName + " has " + wins + " wins";
+                
+                
+        //    }
+        //    //return info;
+        //}
+
+        public string GetName(string playername)
+        {
+            string name = "Player not connected yet";
+            foreach (Player pl in listOfPlayers)
+            {
+                if (pl.PlayerName != playername)
+                {
+                    return pl.PlayerName;
+                }
+                
+            }
+            return name;
+        }
+
+        
+
+        public void AddPlayerToList(string playername)
+        {
+            Player tempPlayer = new Player(playername);
+            tempPlayer.callback = OperationContext.Current.GetCallbackChannel<ITictactoeCallback>();
+            listOfPlayers.Add(tempPlayer);
+            
+
+            
+        }
+
+        public List<Player> GetListOfPlayers()
+        {
+            return listOfPlayers;
+        }
+
+
+        public void FireJoinGameEvent(string username)
+        {
+            e_Joingame(username);
+        }
+
+        public void SubscribeToGame()
+        {
+            ITictactoeCallback subscriber = OperationContext.Current.GetCallbackChannel<ITictactoeCallback>();
+            e_Joingame += subscriber.onLoggingIn;
+        }
+    }
+}
---
+++ c#/MDW_Tictactoe/Tictactoe_contract/ITictactoe.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Tictactoe_contract
+{
+    
+    [ServiceContract (CallbackContract = typeof(ITictactoeCallback))]
+    public interface ITictactoe
+    {
+        [OperationContract]
+        void MakeMove(string username, int buttonId, string XO);
+
+        [OperationContract]
+        string GetName(string username);
+
+        [OperationContract]
+        void AddPlayerToList(string playername);
+
+        [OperationContract(IsOneWay = true)]
+        void SubscribeToGame();
+
+        [OperationContract(IsOneWay = true)]
+        void FireJoinGameEvent(string username);
+
+        [OperationContract]
+        void Chat(string username, string message);
+
+        //[OperationContract]
+        //void CountWins(string username, int wins);
+
+    }
+
+    [DataContract]
+    public class Player
+    {
+        [DataMember]
+        public string PlayerName { get; set; }
+        [DataMember]
+        public int PlayerScore { get; set; }
+
+        [DataMember]
+        public ITictactoeCallback callback { get; set; }
+
+        public Player(string PlayerName)
+        {
+            this.PlayerName = PlayerName;
+        }
+    }
+
+    public interface ITictactoeCallback
+    {
+        [OperationContract (IsOneWay=true)]
+        void onLoggingIn(string username);
+        [OperationContract(IsOneWay = true)]
+        void OnChatCallback(string username, string message);
+        [OperationContract(IsOneWay = true)]
+        void OnMoveCallback(int buttonId, string XO);
+        //[OperationContract(IsOneWay = true)]
+        //void WinsCallBack(string username, int wins);
+    }
+}
