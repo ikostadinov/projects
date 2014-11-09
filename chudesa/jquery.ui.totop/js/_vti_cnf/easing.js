86f804e48b22db21ec55020a9dd3a3488b85a0aa
Ivan Kostadinov <ikostadinov93@gmail.com>
2014-11-09 23:38:51 +0200

    project

--- chudesa/jquery.ui.totop/js/_vti_cnf/easing.js
+++
@@ -1,4 +0,0 @@
-vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|02 Sep 2009 11:15:32 -0000
-vti_extenderversion:SR|6.0.2.8161
-vti_backlinkinfo:VX|chudesa/chichen_ica.html chudesa/petra.html chudesa/index.htm chudesa/machu_pikchu.html chudesa/redeemer.html chudesa/stena.html chudesa/tadj_mahal.html chudesa/kolizeum.html
